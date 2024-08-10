4b916c380b213366805c565c435b26b746e1ca61
Éverton Alves <140563844+carloseverton96@users.noreply.github.com>
2024-08-10 18:30:47 -0300

    header e pages

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import { Card,CardContent } from "./card";
+import { Card, CardContent } from "./card";
 import { Button } from "./button";
 import { MenuIcon } from "lucide-react"
 
 const Header = () => {
     return ( 
         <Card>
-            <CardContent>
+            <CardContent className="p-5 flex flex-row items-center justify-between">
                 <Image alt="FSW BARBER" src="/logo.png" height={18} width={120} />
                 <Button size="icon" variant="outline" >
                     <MenuIcon />
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  return <div>
-    {/* Header */}
+  return (
+  <div>
+    <Header />
+    <div className="p-5">
+      <h2 className="text-xl font-bold">Olá, Felipe</h2>
+      <p>Segunda-feira, 05 de agosto.</p>
 
+      <div className="mt-6 flex items-center gap-2">
+        <Input placeholder="Faça sua busca..."/>
+        <Button>
+          <SearchIcon />
+        </Button>
+      </div>
 
-  </div>>
+      <div>
+        <Image 
+        alt="Agende nos melhores com FSW Barber"
+        src="/banner-01.png"
+        fill
+        className="rounded-xl object-cover"
+        />
+      </div>
+    </div>
+
+  </div>
+  )
 }
 
 export default Home
